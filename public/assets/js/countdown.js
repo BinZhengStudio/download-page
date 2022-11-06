a80a69f4b7c3eb9ebe754f39be19f99b01ce1711
vvvbbbcz <vvvbbbcz@163.com>
2022-11-06 15:15:48 +0800

    修正链接

--- public/assets/js/countdown.js
+++ public/assets/js/countdown.js
@@ -2,10 +2,11 @@ window.onload = function () {
     let future = new Date(new Date().getTime() + 5000)
     let interval = setInterval(function () {
         const second = $('.second');
+        const link = $('.download-link a')[0];
         if (future.getSeconds() - new Date().getSeconds() <= 0) {
-            second.text(0)
-            clearInterval(interval)
-            $('.card a')[0].click()
+            second.text(0);
+            clearInterval(interval);
+            link.click();
             return;
         }
         second.text(future.getSeconds() - new Date().getSeconds())
