d835efc10f9536e8a94c3c6b55318f27e5aebcb0
3527231 <2507274689@qq.com>
2022-11-06 12:33:52 +0800

    基础代码

--- router/getDownLoad.js
+++ router/getDownLoad.js
@@ -1,21 +1,32 @@
 const express = require('express');
-const path = require('path');
+let axios = require('axios');
 let router = express.Router();
 const template = require('art-template');
+const path = require("path");
 
 
 
 router.get('/download/:modName/:minecraftVersion/:modVersion',(req, res)=>{
-    let param = req.params
-    let downLoadUrl;
-    let fileName;
-    let html = template(path.join(__dirname,"../view/index.art"),{
-        data:{
-            download:downLoadUrl,
-            fileName:fileName
-        }
+    let param = req.params;
+    let modName = param.modName
+    let minecraftVersion = param.minecraftVersion
+    let modVersion = param.modVersion
+    let fileName = `${modName}-${minecraftVersion}-${modVersion}.jar`;
+    let downLoadUrl = `https://mirror-name1.maven.bzgzs.cn/cn/bzgzs/${modName}/${modName}-${minecraftVersion}/${modVersion}/${fileName}`;
+    axios.get(downLoadUrl).then(function (){
+        let html = template(path.join(__dirname,"../view/index.art"),{
+            data:{
+                download:downLoadUrl,
+                fileName:fileName
+            }
+        })
+        res.send(html)
+    }).catch(() => {
+        let html = template(path.join(__dirname,"../view/404.art"),{
+            data:{}
+        })
+        res.send(html)
     })
-    res.send(html)
 })
 
 module.exports = router;
