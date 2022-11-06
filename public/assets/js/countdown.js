c717765c51a06df9c4edd32053497b77056a5587
vvvbbbcz <vvvbbbcz@163.com>
2022-11-06 14:31:25 +0800

    基本可用

--- public/assets/js/countdown.js
+++ public/assets/js/countdown.js
@@ -1,12 +1,13 @@
-window.onload = function (){
-    let future = new Date(new Date().getTime()+5000)
-    let interval = setInterval(function (){
-        if (future.getSeconds() - new Date().getSeconds() <= 0){
-            $('.second').text(0)
+window.onload = function () {
+    let future = new Date(new Date().getTime() + 5000)
+    let interval = setInterval(function () {
+        const second = $('.second');
+        if (future.getSeconds() - new Date().getSeconds() <= 0) {
+            second.text(0)
             clearInterval(interval)
             $('.card a')[0].click()
             return;
         }
-        $('.second').text(future.getSeconds() - new Date().getSeconds())
-    },1000)
+        second.text(future.getSeconds() - new Date().getSeconds())
+    }, 1000)
 }
--- router/getDownload.js
+++ router/getDownload.js
@@ -5,25 +5,24 @@ const template = require('art-template');
 const path = require("path");
 
 
-
-router.get('/download/:modName/:minecraftVersion/:modVersion',(req, res)=>{
+router.get('/download/:modName/:minecraftVersion/:modVersion', (req, res) => {
     let param = req.params;
     let modName = param.modName
     let minecraftVersion = param.minecraftVersion
     let modVersion = param.modVersion
     let fileName = `${modName}-${minecraftVersion}-${modVersion}.jar`;
-    let downloadUrl = `https://mirror-name1.maven.bzgzs.cn/cn/bzgzs/${modName}/${modName}-${minecraftVersion}/${modVersion}/${fileName}`;
-    axios.get(downloadUrl).then(function (){
-        let html = template(path.join(__dirname,"../view/index.art"),{
-            data:{
-                download:downloadUrl,
-                fileName:fileName
+    let downloadUrl = `https://maven.bzgzs.cn/cn/bzgzs/${modName}/${modName}-${minecraftVersion}/${modVersion}/${fileName}`;
+    axios.get(downloadUrl).then(function () {
+        let html = template(path.join(__dirname, "../view/index.art"), {
+            data: {
+                download: downloadUrl,
+                fileName: fileName
             }
         })
         res.send(html)
     }).catch(() => {
-        let html = template(path.join(__dirname,"../view/404.art"),{
-            data:{}
+        let html = template(path.join(__dirname, "../view/404.art"), {
+            data: {}
         })
         res.send(html)
     })
