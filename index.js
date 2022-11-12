f786672098ac9a02850e6b90be6f7f81fde25e23
vvvbbbcz <vvvbbbcz@163.com>
2022-11-12 18:07:52 +0800

    修正错误

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const template = require('art-template')
 const app = express()
 
 app.use(router)
-app.use(express.static(__dirname+'\\public\\assets'));
+app.use(express.static(__dirname+'/public/assets'));
 app.use(function (req, res, next) {
     res.send(template(path.join(__dirname, "view/404.art"),{data:{}}))
 })
