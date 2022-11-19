7cfb5f7ed9b48308c6b49871854a0953429c7543
vvvbbbcz <vvvbbbcz@163.com>
2022-11-19 23:43:06 +0800

    静态生成完善

--- src/generator.js
+++ src/generator.js
@@ -1,15 +1,28 @@
 const template = require('art-template');
 const fs = require('fs');
 
+const writeFileRecursive = function(path, buffer, callback){
+    let lastPath = path.substring(0, path.lastIndexOf("/"));
+    fs.mkdir(lastPath, {recursive: true}, (err) => {
+        if (err) return callback(err);
+        fs.writeFile(path, buffer, function(err){
+            if (err) return callback(err);
+            return callback(null);
+        });
+    });
+}
+
 function generateStatic(version) {
     const path = require('path')
     let html = template(path.join(__dirname, "../view/index.html"), {
         file: "test",
         url: "test"
     })
-    fs.writeFile(`build/test/${version}/index.html`, html, err => {
-        console.log(err);
-    })
+    fs.mkdir(`build/test/${version}`, {recursive: true}, () => {
+        fs.writeFile(`build/test/${version}/index.html`, html, err => {
+            if (err) console.log(err);
+        })
+    });
 }
 
 module.exports = {
