7294468b4c3812aff9f1e4af05e7b9b2a4ae3701
vvvbbbcz <vvvbbbcz@163.com>
2023-01-22 17:07:03 +0800

    修改文件结构

--- src/generator.js
+++ src/generator.js
@@ -2,7 +2,7 @@ const template = require('art-template');
 const fs = require('fs');
 const path = require("path");
 
-function genMainPage() {
+function genMainPage() { // 生成主页
     let html = template(path.join(__dirname, "../view/index.html"), {})
     fs.mkdir(`build/`, {recursive: true}, () => {
         fs.writeFile(`build/index.html`, html, err => {
@@ -12,33 +12,32 @@ function genMainPage() {
 }
 
 function genVersion(modName, mcVersion, modVersion) {
-    const path = require('path')
-    let file = `${modName}-${mcVersion}-${modVersion}.jar`;
-    let url = `https://maven.bzgzs.cn/cn/bzgzs/${modName}/${modName}-${mcVersion}/${modVersion}/${file}`;
-    let html = template(path.join(__dirname, "../view/download.html"), {
-        file: file,
-        url: url
-    })
     fs.mkdir(`build/${modName}/${mcVersion}/${modVersion}`, {recursive: true}, () => {
-        fs.writeFile(`build/${modName}/${mcVersion}/${modVersion}/index.html`, html, err => {
+        fs.writeFile(`build/${modName}/${mcVersion}/${modVersion}/index.html`, genPage(modName, mcVersion, modVersion), err => {
             if (err) console.log(err);
         })
     });
 }
 
 function genLatest(modName, mcVersion, modVersion) {
+    fs.mkdir(`build/${modName}/${mcVersion}/latest`, {recursive: true}, () => {
+        fs.writeFile(`build/${modName}/${mcVersion}/latest/index.html`, genPage(modName, mcVersion, modVersion), err => {
+            if (err) console.log(err);
+        })
+    });
+}
+
+function genPage(modName, mcVersion, modVersion) {
     const path = require('path')
     let file = `${modName}-${mcVersion}-${modVersion}.jar`;
     let url = `https://maven.bzgzs.cn/cn/bzgzs/${modName}/${modName}-${mcVersion}/${modVersion}/${file}`;
+    let header = template(path.join(__dirname, "../view/header.html"), {})
+    let footer = template(path.join(__dirname, "../view/footer.html"), {})
     let html = template(path.join(__dirname, "../view/download.html"), {
         file: file,
         url: url
     })
-    fs.mkdir(`build/${modName}/${mcVersion}/latest`, {recursive: true}, () => {
-        fs.writeFile(`build/${modName}/${mcVersion}/latest/index.html`, html, err => {
-            if (err) console.log(err);
-        })
-    });
+    return header + html + footer;
 }
 
 module.exports = {
